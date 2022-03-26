56f1c228cae189775aa4053fbf8e7404988b1fe1
adebayo-adewunmi <adebayo.adewunmi@africaprudential.com>
2022-03-26 15:40:07 +0100

    minor

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -99,6 +99,16 @@ const Sidebar = (props :any) => {
                     </Link>
                 </li>
             </ul>
+
+            <a href="https://merchant.payflexi.co/" className="-mx-3 px-3 py-2 mb-2 flex items-center justify-between text-sm font-medium rounded-lg bg-indigo-100 border border-indigo-200 menu-link-active" >
+                <span className="inline-flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-6 w-6 transition-all ease-out transition-medium">
+                        <path d="M21 14v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5h3a1 1 0 00.83-.45L10 8.8l5.17 7.75a1 1 0 001.66 0l1.7-2.55H21z" className="ic-sec"></path> 
+                        <path d="M21 12h-3a1 1 0 00-.83.45L16 14.2l-5.17-7.75a1 1 0 00-1.66 0L5.47 12H3V5c0-1.1.9-2 2-2h14a2 2 0 012 2v7z" className="ic-pri"></path>
+                    </svg> 
+                    <span className="ml-2 text-indigo-700 font-semibold">Dashboard</span>
+                </span>
+            </a>
         </div>
     );
 };
