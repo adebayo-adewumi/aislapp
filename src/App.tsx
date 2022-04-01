4ed13e8dad6c23610d94a88c60c86aefc7ebb8e1
adebayo-adewunmi <adebayo.adewunmi@africaprudential.com>
2022-04-01 21:23:54 +0100

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { Layout } from 'antd';
 import React from 'react';
 import Main from './containers/Main';
 
 
 const App = () => {
   return (
-    <Layout>
       <Main />
-    </Layout>
   );
 }
 
